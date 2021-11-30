38fc4bb988fe1b765f591eb44dce3fcc8f07dfd8
m-bashari-m <89944686+m-bashari-m@users.noreply.github.com>
2021-12-01 01:41:21 +0330

    Update q4.cpp

--- pro006/q4.cpp
+++ pro006/q4.cpp
@@ -1,33 +1,93 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+class EmployeeException : public exception {
+public:
+    explicit EmployeeException(string err);
+
+    const char *what() const noexcept override;
+
+private:
+    string m_error;
+    int m_working_hour;
+};
+
+EmployeeException::EmployeeException(string err) {
+    m_error = err;
+}
+
+const char *EmployeeException::what() const noexcept {
+    return m_error.c_str();
+}
+
 class Employee {
 public:
+    Employee(string name);
+    
     virtual int earning(int hour) const = 0;
 
 protected:
-    const static long int salary = 14000000;
+    string m_name;
 };
+
+Employee::Employee(string name) {
+    m_name = name;
+}
 
 class FullTime : public Employee {
 public:
+    FullTime(string name);
+
     int earning(int hour) const override;
 
 private:
-    const static int working_hour = 140;
+    const static long m_salary = 14000000;
+    const static int m_working_hour = 140;
 };
 
 int FullTime::earning(int hour) const {
-    return salary + (salary / working_hour) * 3 / 2 * (working_hour - hour);
+    if (hour < 0) {
+        throw EmployeeException("Full time minus working hour");
+    } else if (hour < 140) {
+        throw EmployeeException("Full time employee less than 140 hours work");
+    }
+    return m_salary + (m_salary / m_working_hour) * 3 / 2 * (m_working_hour - hour);
 }
+
+FullTime::FullTime(string name) : Employee(name) {}
 
 class PartTime : public Employee {
 public:
+    PartTime(string name);
+
     int earning(int hour) const override;
+
+private:
+    // It's likely different from full time employee's salary
+    const static long m_salary_per_hour = 200000;
 };
 
 int PartTime::earning(int hour) const {
-    return salary * hour;
+    if (hour < 0) {
+        throw EmployeeException("Part time minus working hour");
+    }
+    return m_salary_per_hour * hour;
 }
 
+PartTime::PartTime(string name) : Employee(name) {};
+
+int main() {
+    FullTime test("test");
+    Employee *Test = &test;
+    Employee *ali = new FullTime("Ali");
+    Employee *reza = new PartTime("reza");
+    Employee *ahmad = new PartTime("ahmad");
+    Employee *sara = new FullTime("sara");
+    vector<Employee *> employees{ali, reza, ahmad, sara, Test};
+    for (auto &employee: employees) {
+        cout << employee->earning(140) << endl;
+        delete employee;
+    }
+}
